cf1372eb3ab941e9864b16c6758e3e99200aa250
Walter Barbieri <walter90@hotmail.it>
2023-07-28 10:50:05 +0200

    Primo Commit

---
+++ src/main/java/w17progetto/W17progettoApplication.java
@@ -0,0 +1,13 @@
+package w17progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W17progettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W17progettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/w17progetto/W17progettoApplicationTests.java
@@ -0,0 +1,13 @@
+package w17progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W17progettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
